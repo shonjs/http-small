5209534d67409b9f9e09dffdae662d6e802a8cbb
shonjs <rush2shon@gmail.com>
2019-07-24 21:27:06 +0530

    [WIP] Add port option from commandline

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,30 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::fs::File;
 use std::path::Path;
+use std::env;
+
+extern crate getopts;
+use getopts::Options;
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+    let args: Vec<_> = env::args().collect();
+
+    let mut opts = Options::new();
+    opts.optopt("p", "port", "listening port", "PORT");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(matches) => {matches},
+        Err(err) => { panic!("Error: {}", err) }
+    };
+
+    if !matches.opt_present("p") {
+        println!("Port needs to be specified");
+        return
+    }
+
+    let port: u32 = matches.opt_str("p").unwrap().parse::<u32>().unwrap();
+    let bind_address: String = String::from("0.0.0.0:").push_str(&port.to_string());
+    let listener = TcpListener::bind(bind_address).unwrap();
     println!("Listening on port 8080");
 
     for stream in listener.incoming() {
@@ -39,24 +60,24 @@ fn handle_read(mut stream: &TcpStream) {
 }
 
 fn read_response(mut stream: TcpStream) {
-    let file = File::open(Path::new("../assets/image.jpeg"));
+    let file = File::open(Path::new("./assets/test.txt"));
     // let mut buffer = [0;4096];
 
     // file.read(&mut buffer);
 
     // let mut buffer = Vec::new();
     // file.read_to_end(&mut buffer)?;
-
+    let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Hello world</body></html>\r\n";
     let mut buffer = String::new();
     match file {
         Ok(mut reader) => {
             reader.read_to_string(&mut buffer).unwrap();
-            let response = buffer.as_bytes();
+            // let response = buffer.as_bytes();
             match stream.write(response) {
                 Ok(_) => println!("Response sent"),
                 Err(error) => println!("failed sending response")
             }
         }
-        Err(error) => println!("Error in opening file")
+        Err(error) => println!("Error in opening file {}", error)
     }
 }
