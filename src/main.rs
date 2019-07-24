8e05799297eaaa15c5a0984d21ed667eeb84f14a
shonjs <rush2shon@gmail.com>
2019-07-24 08:19:31 +0530

    [WIP] giving back respons

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
+use std::fs::File;
+use std::path::Path;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
@@ -9,13 +11,18 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                thread::spawn(move || handle_read(&stream));
+                thread::spawn(move || handle_request(stream));
             }
             Err(err) => {
                 println!("Error in connection {}", err);
             }
         }
     }
+}
+
+fn handle_request(stream: TcpStream){
+    handle_read(&stream);
+    read_response(stream)
 }
 
 fn handle_read(mut stream: &TcpStream) {
@@ -28,5 +35,28 @@ fn handle_read(mut stream: &TcpStream) {
         Err(err) => {
             println!("Reading stream failed {}", err);
         }
+    }
+}
+
+fn read_response(mut stream: TcpStream) {
+    let file = File::open(Path::new("../assets/image.jpeg"));
+    // let mut buffer = [0;4096];
+
+    // file.read(&mut buffer);
+
+    // let mut buffer = Vec::new();
+    // file.read_to_end(&mut buffer)?;
+
+    let mut buffer = String::new();
+    match file {
+        Ok(mut reader) => {
+            reader.read_to_string(&mut buffer).unwrap();
+            let response = buffer.as_bytes();
+            match stream.write(response) {
+                Ok(_) => println!("Response sent"),
+                Err(error) => println!("failed sending response")
+            }
+        }
+        Err(error) => println!("Error in opening file")
     }
 }
