2cbc11f033135af92522bcae78e6cf9a6e380d90
shonjs <rush2shon@gmail.com>
2019-07-29 21:09:06 +0530

    [WIP] Adding client instances to readiness events from the server module

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,10 @@
 use std::fmt;
+use std::net::SocketAddr;
+use std::collections::HashMap;
+
+extern crate mio;
+use mio::*;
+use mio::net::{TcpListener};
 
 pub struct Server {
     host_name: String,
@@ -12,6 +18,19 @@ impl Server {
 
     pub fn run(&self) {
         println!("The server is running");
+        let mut address: String = self.host_name.clone();
+        address.push_str(":");
+        address.push_str(&self.port_number.to_string());
+        let mut socket_address: SocketAddr = address.parse().unwrap();
+        // Bind listening socket
+        let server = TcpListener::bind(&socket_address).unwrap();
+
+        // Create event listner using tokens
+        let SERVER_TOKEN: Token = Token(0);
+        let poll = Poll::new().unwrap();
+        poll.register(&server, SERVER_TOKEN, Ready::readable(), PollOpt::edge()).unwrap();
+        let mut events = Events::with_capacity(1024);
+
     }
 }
 
